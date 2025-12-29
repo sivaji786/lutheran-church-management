e5ed08fca2ded8f67767f749a209e0138d02d71a
Sivaji Kanchibhotla <sivaji786@gmail.com>
2025-12-29 11:29:40 +0530

    deleteing not needed files

--- _archive_old_files/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Build script for Lutheran Church Management System
-rem Robust to being called from any location
-
-cd /d %~dp0
-call npm run build
-echo Build complete. Files are in the 'build' directory.
-pause
--- _archive_old_files/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build script for Lutheran Church Management System
-# Robust to being called from any location
-
-set -e
-cd "$(dirname "$0")"
-npm run build
-echo "Build complete. Files are in the 'build' directory."
